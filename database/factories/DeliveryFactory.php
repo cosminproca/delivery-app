c625fdf1b9cd6b017f720efc6eff11d103f0b55b
Cosmin Proca <proca.cosmin.dumitru@gmail.com>
2021-07-22 23:40:34 +0300

    Added historical data

---
+++ database/factories/DeliveryFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Delivery;
+use Carbon\Carbon;
+use Carbon\Traits\Creator;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DeliveryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Delivery::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $shipment_date = new Carbon($this->faker->dateTimeBetween('-1 years'));
+        $delivery_date = Carbon::parse($shipment_date)->addWeekDays($this->faker->numberBetween(3,14));
+
+        return [
+            'zip_code' => $this->faker->randomElement(['030993', '312545', '754323', '461235', '163267']),
+            'shipment_date' => $shipment_date,
+            'delivery_date' => $delivery_date,
+        ];
+    }
+}
--- database/seeders/DeliverySeeder.php
+++ database/seeders/DeliverySeeder.php
@@ -2,19 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Delivery;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class DeliverySeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-           DeliverySeeder::class
-        ]);
+        Delivery::factory(10)->create();
     }
 }
